1b2e5ef72e2c1ea7bbb0638bae4f7b9b9f8e9fbc
Emmanuel Villegas <emmanuelvillegasgonzalez@gmail.com>
2020-02-13 16:58:33 -0400

    promesas y async await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -31,3 +31,34 @@ test("Probar un callback", () => {
     expect(str).toBe("aloH");
   });
 });
+
+const reverseString2 = str => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error("Error"));
+    }
+    resolve(
+      str
+        .split("")
+        .reverse()
+        .join("")
+    );
+  });
+};
+
+test("Probar una promesa", () => {
+  return reverseString2("Hola").then(str => {
+    expect(str).toBe("aloH");
+  });
+});
+
+test("Probar async/await", async () => {
+  const str = await reverseString2("Hola");
+  expect(str).toBe("aloH");
+});
+
+beforeEach(() => console.log("Antes de cada prueba"));
+beforeAll(() => console.log("Antes de todas las pruebas"));
+
+afterEach(() => console.log("Despues de cada prueba"));
+afterAll(() => console.log("Despues de todas las pruebas"));
