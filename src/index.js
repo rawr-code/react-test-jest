c88ba7e8fca1c35025c5956e31c496d053096912
Emmanuel Villegas <emmanuelvillegasgonzalez@gmail.com>
2020-02-16 11:37:10 -0400

    add more test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-const cities = ["Ciudad de México", "Bogotá", "Lima", "Buenos Aires"];
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './routes/App';
+import reducer from './reducers';
 
-const randomString = () => {
-  const str = cities[Math.floor(Math.random() * cities.length)];
-  console.log(str);
-  return str;
-};
+import initialState from './initialState';
 
-const reverseString2 = str => {
-    return new Promise((resolve, reject) => {
-      if (!str) {
-        reject(Error("Error"));
-      }
-      resolve(
-        str
-          .split("")
-          .reverse()
-          .join("")
-      );
-    });
-  };
-  
-module.exports = randomString;
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
